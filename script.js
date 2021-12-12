66b0d9232b36922f86321f558313320ba900cd88
zhouxinlu714 <daisyzhou714@gmail.com>
2021-12-11 23:36:48 -0500

    assurant

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-    });
-    //toggle menu
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-});
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+//         }else{
+//             $('.navbar').removeClass("sticky");
+//         }
+//     });
+//     //toggle menu
+//     $('.menu-btn').click(function(){
+//         $('.navbar .menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");
+//     });
+// });
+AOS.init();
